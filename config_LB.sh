eff794045aeadfdb9ba2bf4dcd28b303340a2076
ejmalfatti <ejmalfatti@outlook.com>
2015-01-22 09:36:59 -0300

    Rama de prueba, modificaciones en todas las funciones y menu principal

--- config_LB.sh
+++ config_LB.sh
@@ -13,16 +13,28 @@ function crear_Dir(){
 }
 
 function lb_Espaniol(){
+	clear
+	echo " Instalando el idioma español para LibreOffice"
+	echo; sleep 1
 	apt-get -y install libreoffice-l10n-es
+	clear
 }
 
 function lb_Ortografia(){
+	clear
+	echo " Instalando los diccionarios desde repositorios"
+	echo; sleep 1
 	apt-get -y install myspell-es aspell aspell-es
+	clear
 }
 
 function lb_LanguageTool(){
 	#sudo apt-get -y install openoffice.org-java-common # Java para OpenOffice 
+	clear
+	echo " Instalando Java para LibreOffice"
+	echo; sleep 1
 	apt-get -y install libreoffice-java-common # Java para LibreOffice
+	
 	DOWN="http://www.languagetool.org/download/LanguageTool-2.8.oxt"
 
 	crear_Dir # llamando a la funcion que crea el directorio tmporal	
@@ -31,18 +43,19 @@ function lb_LanguageTool(){
 	
 	if [ "$dirActual" == "$HOME/libreoffice" ]; then
 		if [ -f "$HOME/LanguageTool-2.8.oxt" ]; then
-			echo "El fichero \"LanguageTool-2.8.oxt\" existe"
+			echo " El fichero \"LanguageTool-2.8.oxt\" existe"
 		else
-			echo "Descargando la extensión LanguageTool-2.8.oxt..."
+			echo " Descargando la extensión LanguageTool-2.8.oxt..."
 			echo; sleep 1
 			wget -c $DOWN
 		fi
 	else
-		echo "No se ha podido descargar la extensión..."
+		echo " No se ha podido descargar la extensión..."
 	fi	
 }
 
 function lb_Extensiones(){
+	clear
 	es_AR="http://ufpr.dl.sourceforge.net/project/aoo-extensions/1503/3/diccionarios-argentina_1.oxt"
 	es_ES_AL="http://extensions.libreoffice.org/extension-center/diccionario-espanol/releases/1.0/spanish-dictionary.oxt"
 	es_ES="subirlo a copy"
@@ -60,53 +73,82 @@ function lb_Extensiones(){
 		clear
 		case $opt in
 				1)
+					clear
+					echo "Descargando la extensión español Argentino..."
+					echo; sleep 1
 					wget -c $es_AR
 					clear
 					;;
 				2)
+					clear
+					echo "Descargando diccionario Español y America Latina..."
+					echo; sleep 1
 					wget -c $es_ES_AL
 					clear
 					;;
 				3)
+					clear
+					echo "Descargando diccionario español Español..."
+					echo; sleep 1
 					wget -c $es_ES
 					clear
+					;;
+				4)
+					break
 					;;
 		esac		
 done
 }
 
 function lb_Apariencia(){
+	clear
+	echo "Instalando los iconos para LibreOffice"
+	echo; sleep 1
 	apt-get -y install libreoffice-style-crystal libreoffice-style-galaxy libreoffice-style-hicontrast libreoffice-style-human libreoffice-style-oxygen libreoffice-style-sifr libreoffice-style-tango
 }
+function main(){
+	TRUE="true"
+	while [ "$TRUE" == "true" ]; do
+		echo "      ....:::: Menú principal ::::...."
+		echo
+		echo " 1) Instalar el idioma español en Libre Office"
+		echo " 2) Instalar los diccionarios desde los repositorios"
+		echo " 3) Descargar extension LanguageTool para Libre Office (50 MB aprox.)"
+		echo " 4) Descargar extensiones para LibreOffice (2 MB c/u aprox.)"
+		echo " 5) Instalar set de iconos para Libre Office"
+		echo " 6) Salir de este menú"
+		echo
+		local opt
+		read -p " Elija la opcion que desea ejecutar: " opt
+		clear
+		case $opt in
+			1)
+				lb_Espaniol
+				;;
+			2)
+				lb_Ortografia
+				;;
+			3)
+				lb_LanguageTool
+				;;
+			4)
+				lb_Extensiones
+				;;
+			5)
+				lb_Apariencia
+				;;
+			6)
+				echo "Saliendo..."; sleep 2
+				clear
+				exit 0
+				;;
+		esac
+	done
+}
 
-TRUE="true"
-while [ "$TRUE" == "true" ]; do
-	echo "      ....:::: Menú principal ::::...."
-	echo
-	echo " 1) Instalar el idioma español en Libre Office"
-	echo " 2) Instalar los diccionarios desde los repositorios"
-	echo " 3) Descargar extensiones para Libre Office (Ortográfia)"
-	echo " 4) Instalar set de iconos para Libre Office"
-	echo " 5) Salir de este menú"
-	echo
-	read -p " Elija la opcion que desea ejecutar: " opt
-	clear
-	case $opt in
-		1)
-			;;
-		2)
-			;;
-		3)
-			;;
-		4)
-			;;
-		5)
-			echo "Saliendo..."; sleep 2
-			clear
-			exit 0
-			;;
-	esac
-done
+# Bloque principal. Llamando a la función main()
+
+main
 
 #	Fuentes:
 #	http://extensions.libreoffice.org/extension-center/languagetool
